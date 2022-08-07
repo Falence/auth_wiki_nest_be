6abf4309bce6113749145d0f9033d7ba7a1df3d1
Falence Lemungoh <falencelemungoh@gmail.com>
2022-08-07 14:33:17 +0100

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './exception.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix('api');
   app.enableCors();
   // app.useGlobalFilters(new AllExceptionsFilter());
